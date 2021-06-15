f3b982a54a6bcf5db0c29e670aef3b6af001d2f7
jgpp <38386145+jguilhermepaiva@users.noreply.github.com>
2021-06-15 19:53:28 -0300

    Update transformacoes_lineares.py

--- src/transformacoes_lineares.py
+++ src/transformacoes_lineares.py
@@ -4,6 +4,9 @@ from ferramentas_basicas import reflexao as reflexao_vetor
 def reflexao_reta(vetor, vetorDiretor):
     return reflexao_vetor(vetor, vetorDiretor)
 
+def reflexao_plano(vetor, vetorNormal):
+    return reflexao_vetor(vetor, vetorNormal)
+
 def reflexao(vetor):
     return Vetor(-1*vetor.x, -1*vetor.y, -1*vetor.z)
 
